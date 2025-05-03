cff3b62e33d692a0a68e02a115183f0078992d07
YaqubRahman <YaqubRahman999@gmail.com>
2025-05-03 13:35:18 +0100

    feat: notes data storing set up + routes for notes started

---
+++ backend/src/db/notesData.ts
@@ -0,0 +1,10 @@
+interface Notes {
+  id: number;
+  text: string;
+  username: string;
+  timestamp: number;
+}
+
+const notes: Notes[] = [];
+
+export default notes;
---
+++ backend/src/routes/notesRoutes.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+const express = require("express");
+const router = express.Router();
+
+router.post("/notes", async (req: Request, res: Response) => {});
+
+router.get("/notes", async (req: Request, res: Response) => {});
